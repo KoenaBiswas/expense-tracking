a9d17e7a4e9d32cabdfcd598c2db1962b3087861
Koena Biswas <144685053+KoenaBiswas@users.noreply.github.com>
2026-03-23 14:18:27 +0530

    Change title from 'Last 30 days expenses' to 'Last 3000 weeks expenses'

--- frontend/expense-tracker/src/components/Dashboard/Last30DaysExpenses.jsx
+++ frontend/expense-tracker/src/components/Dashboard/Last30DaysExpenses.jsx
@@ -14,7 +14,7 @@ const Last30DaysExpenses = ({ data }) => {
   return (
     <div className="card col-span-1">
       <div className="flex items-center justify-between">
-        <h5 className="text-lg">Last 30 days expenses</h5>
+        <h5 className="text-lg">Last 3000 weeks expenses</h5>
       </div>
 
       <CustomBarChart data = {chartData}/>

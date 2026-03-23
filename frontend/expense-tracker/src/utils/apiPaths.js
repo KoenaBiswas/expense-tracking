f5ca383370e82ede5c6c124cc7610fe6959093f6
Koena Biswas <144685053+KoenaBiswas@users.noreply.github.com>
2026-03-23 11:29:12 +0530

    Update BASE_URL for expense tracker API

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://expense-backend-gzom.onrender.com";
+export const BASE_URL = "https://expense-tracking-lbk6.onrender.com";
 // utils/apiPaths.js
 export const API_PATHS = {
   AUTH: {
